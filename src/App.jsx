d4f14fa494b46b39ec5896920c87f4ebebf6b9de
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-04-08 16:47:35 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Hero />
+      <Projects />
+      <Footer />
+    </div>
+  )
+}
